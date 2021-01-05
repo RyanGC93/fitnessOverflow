f75f6b4e8c1a8edcb47a8ffad8669f28f2f37519
Luke <lukebraun88@att.net>
2021-01-05 12:57:20 -0600

    finished askQuestions

--- routes/questions.js
+++ routes/questions.js
@@ -11,12 +11,11 @@ const { check, validationResult } = require('express-validator');
 router.get('/ask', csrfProtection, (req,res)=>{
     const question = db.Question.build()
     res.render('create-question', {
-        title: 'Ask a public Question',
+        title: 'Ask A Public Question',
         question,
         csrfToken: req.csrfToken()
     })
 })
-
 
 const questionValidators = [
     check('title')
@@ -30,3 +29,35 @@ const questionValidators = [
         .isLength({ max: 255 })
         .withMessage('Question must be less than 255 characters'),
 ]
+
+
+router.post('/ask', csrfProtection, questionValidators, asyncHandler(async (req, res) => {
+    const {
+        title,
+        body
+    } = req.body
+
+    const question = db.Question.build({
+        title,
+        body
+    })
+
+    const validatorErrors = validationResult(req)
+
+    if (validatorErrors.isEmpty()) {
+        await question.save();
+        res.redirect('/')
+    }
+    else {
+        const errors = validatorErrors.array().map((error) => error.msg)
+        res.render('create-question', {
+            title: 'Ask A Public Question',
+            question,
+            errors,
+            csrfToken: req.csrfToken(),
+        })
+    }
+}))
+
+
+module.exports = router
