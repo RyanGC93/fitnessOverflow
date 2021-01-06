587c5549eb58f861103b6a2828917929bb16f609
Evanmhargett <evanmhargett@gmail.com>
2021-01-06 12:57:45 -0500

    finished basic ajax functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,16 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
+window.addEventListener("DOMContentLoaded", (event)=>{
+    let searchBox = document.getElementById('search')
+    let searchButton = document.getElementById('searchButton')
+    searchButton.addEventListener('click', async (event) =>{
+        let res = await fetch(`http://localhost:8080/api/search?search=${searchBox.value}`)
+        let {questions} = await res.json()
+        let questionContainer = document.getElementById('questionContainer')
+        questionContainer.innerHTML = ""
+        questions.forEach(question =>{
+            let ele = document.createElement('li')
+            ele.innerHTML = question.title
+            ele.classList.add('question')
+            questionContainer.appendChild(ele)
+        })
+    })
 })
