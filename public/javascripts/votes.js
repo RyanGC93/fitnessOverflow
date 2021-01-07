fda88b2b68ef9da1c870497c3683a2f60b629cc0
Evanmhargett <evanmhargett@gmail.com>
2021-01-07 11:30:13 -0500

    Finished basic version of upvote/downvote with AJAX

--- public/javascripts/votes.js
+++ public/javascripts/votes.js
@@ -1,9 +1,36 @@
 window.addEventListener("load", (event)=>{
-    let upvote = document.querySelector(".upVote")
+    let questionId = document.getElementById('questionId').innerText
+
+    let votingContainers = document.querySelectorAll(".voting-container")
+
+    votingContainers.forEach(voteContainer =>{
+        let answerId = voteContainer.id
+        let upvoteButton = voteContainer.childNodes[1]
+        let downvoteButton = voteContainer.childNodes[3]
+        
+        upvoteButton.addEventListener("click", async (event) =>{
+            let res = await fetch(`http://localhost:8080/questions/${questionId}/answer/${answerId}/upvote`, {
+                method: "PATCH",
+                headers:
+                {'Content': "application/json"}
+            })
+            const json = await res.json()
+            console.log(json)
+        })
+        downvoteButton.addEventListener("click", async (event) =>{
+            let res = await fetch(`http://localhost:8080/questions/${questionId}/answer/${answerId}/downvote`, {
+                method: "PATCH",
+                headers:
+                {'Content': "application/json"}
+            })
+            const json = await res.json()
+            console.log(json)
+        })
+    })
     let downvote = document.querySelector('.downVote')
 
-    upvote.addEventListener('click', patchHandler('upvote'))
-    downvote.addEventListener('click', patchHandler('downVote'))
+    // upvote.addEventListener('click', patchHandler('upvote'))
+    // downvote.addEventListener('click', patchHandler('downVote'))
 
     async function patchHandler(voteType) {
         fetch('/votes', {
--- routes/likes.js
+++ routes/likes.js
@@ -16,12 +16,31 @@ const voteValidators = [
 ]
 
 
-router.route('/questions/:id(\\d+)/answer/:id2(\\d+)/upvotes')
+router.route('/questions/:id(\\d+)/answer/:id2(\\d+)/upvote')
  	.patch( asyncHandler(async (req,res)=>{
 		 const questionId = parseInt(req.params.id, 10)
 		  const answerId = parseInt(req.params.id2, 10)
 	    const { userId } = req.session.auth
-	    const upvotes = await db.Vote.findAll({
+
+        // //   console.log(downvotes)
+		
+		let existingVote = await db.Vote.findOne({where: {
+			userId,
+	    	answerId
+		}})
+		if(existingVote){
+			await existingVote.destroy()
+		}
+
+	    const vote = await db.Vote.build({
+			userId,
+			answerId,
+			voteType: "upvote"
+		})
+		
+		await vote.save()
+
+		const upvotes = await db.Vote.findAll({
 	    	where: {
 			answerId,
 	    	voteType: "upvote"
@@ -34,23 +53,68 @@ router.route('/questions/:id(\\d+)/answer/:id2(\\d+)/upvotes')
 	    	}
 		})
 
-        //   console.log(downvotes)
-	    let totalUpVotes= upvotes.length + 1
+		let totalUpVotes= upvotes.length
 		let totalDownVotes= downvotes.length
-		let totalVotes = (totalUpvotes - totalDownvotes)
-
-	    const vote = db.Vote.build({
-			userId,
-			answerId,
-			voteType: "upvote"
-		})
-
-		await vote.save()
+		
+		let totalVotes = totalUpVotes - totalDownVotes
 
 		res.json({
 			title: 'Question',
+			totalUpVotes,
+			totalDownVotes,
+			totalVotes,
+			upvotes
+		})
+	}))
+
+	router.route('/questions/:id(\\d+)/answer/:id2(\\d+)/downvote')
+ 	.patch( asyncHandler(async (req,res)=>{
+		 const questionId = parseInt(req.params.id, 10)
+		  const answerId = parseInt(req.params.id2, 10)
+	    const { userId } = req.session.auth
+
+        // //   console.log(downvotes)
+		
+		let existingVote = await db.Vote.findOne({where: {
+			userId,
+	    	answerId
+		}})
+		if(existingVote){
+			await existingVote.destroy()
+		}
+		
+	    const vote = await db.Vote.build({
+			userId,
 			answerId,
-			totalVotes
+			voteType: "downvote"
+		})
+		
+		await vote.save()
+
+		const upvotes = await db.Vote.findAll({
+	    	where: {
+			answerId,
+	    	voteType: "upvote"
+	    	}
+	    })
+	    const downvotes = await db.Vote.findAll({
+	    	where: {
+			answerId,
+	    	voteType: "downvote"
+	    	}
+		})
+
+		let totalUpVotes= upvotes.length
+		let totalDownVotes= downvotes.length
+		
+		let totalVotes = totalUpVotes - totalDownVotes
+
+		res.json({
+			title: 'Question',
+			totalUpVotes,
+			totalDownVotes,
+			totalVotes,
+			upvotes
 		})
 	}))
 
