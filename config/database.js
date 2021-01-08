5394f78e57e3566bba50a7ccb341e1336d915697
Evanmhargett <evanmhargett@gmail.com>
2021-01-08 13:32:54 -0500

    Removed unnecessary change

--- config/database.js
+++ config/database.js
@@ -14,7 +14,6 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    seederStorage: 'sequelize',
-    host
+    seederStorage: 'sequelize'
   }
 };
